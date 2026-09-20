a11013605a9743e151001087c8e7a66afa99727b
hlidesign17-ux <hlidesign17@gmail.com>
2026-09-21 04:19:12 +0700

    pw

--- js/mock-users.js
+++ js/mock-users.js
@@ -1,28 +1,33 @@
-// Data akun otomatis untuk kelas X, XI, dan XII (1-5)
+// Data akun otomatis untuk kelas X1-X5, XI1-XI5, dan XII1-XII5
 function generateUsers() {
   const grades = ["X", "XI", "XII"];
   const classCodes = [
-    { classSuffix: "1", code: "v" },
-    { classSuffix: "2", code: "w" },
-    { classSuffix: "3", code: "x" },
-    { classSuffix: "4", code: "y" },
-    { classSuffix: "5", code: "z" },
+    { num: "1", code: "v" },
+    { num: "2", code: "w" },
+    { num: "3", code: "x" },
+    { num: "4", code: "y" },
+    { num: "5", code: "z" },
   ];
 
   const users = [];
 
   grades.forEach((grade) => {
-    classCodes.forEach(({ classSuffix, code }) => {
+    classCodes.forEach(({ num, code }) => {
+      // Pembentukan nama kelas penuh (misal: "X1", "XI3", "XII5")
+      const fullClassName = `${grade}${num}`;
+
       // Loop untuk 40 siswa per kelas
       for (let i = 1; i <= 40; i++) {
         const numStr = i < 10 ? `0${i}` : `${i}`;
-        const key = `${grade}${code}${numStr}`;
+
+        // Pola baru: edu + X1 + v + 01 -> eduX1v01
+        const key = `${fullClassName}${code}${numStr}`;
 
         users.push({
           username: `edu${key}`,
           password: `alfalah${key}`,
           grade: grade,
-          className: `${grade}-${classSuffix}`, // Contoh output: "X-1", "XI-2", dll.
+          className: fullClassName, // Output: "X1", "X2", "XI1", dll.
         });
       }
     });
